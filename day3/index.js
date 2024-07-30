8613149cc95f38a1755f8c4471d6b2315a6cd9e8
akshaymakwana07 <akshaymakwana2739@gmail.com>
2024-07-30 12:41:18 +0530

    day3

--- day3/index.js
+++ day3/index.js
@@ -1,19 +1,23 @@
 const http = require("http");
-const fsPromise = require("fs/promises");
+const fsPromise = require("fs/promises")
 
-const server = http.createServer(async(req,res)=>{
-    const path = req.url;
-    const method = req.method;
+const server = http.createServer((req,res)=>{
+const path = req.url;
+const method = req.method;
 
-    console.log(new Date(), path, method);
-    if(path.includes("file") && method == "GET"){
-        const filename = path.split("/").pop();
-        console.log(filename);
-        const data = await fsPromise.readFile("./" + filename);
-        res.end(data);
-    }
-})
+console.log(new Date(),path,method)
 
-server.listen(6700,()=>{
+if(path.includes("file") && method == "GET"){
+    const filename = path.split("/").pop();
+    console.log(filename);
+    const data = await fsPromise.readFile("./" + filename)
+    res.end(data)
+}
+});
+
+server.listen(7878,()=>{
     console.log("server running");
-})
+
+  
+   
+});
